f751257b95683d8cb3cace6322aeedde00a2bd46
Tim <timothy.b.stott@gmail.com>
2022-09-27 10:24:05 -0400

    Add Logic for password check.
    If passwords do not match the create account button
    now becomes disabled.

--- main.js
+++ main.js
@@ -1,12 +1,16 @@
 const submitBtn = document.getElementById("submitBtn");
-window.addEventListener('keydown',checkPasswords);
+document.addEventListener('mousemove',checkPasswords);
+document.addEventListener('keydown',checkPasswords);
 
 
 function checkPasswords(){
     let pass1 = document.getElementById("password");
     let pass2 = document.getElementById("password_2");
-    if(pass1!=pass2){
+    if(pass1.value!=pass2.value){
+        submitBtn.disabled = true;
+        console.log(submitBtn);
     }
+
     else{
         submitBtn.disabled = false;
     }
